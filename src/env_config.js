308882ba1708ffd847816a7193a25316affdb11e
KenTandrian <kenricktan11@gmail.com>
2022-05-13 23:38:50 +0700

    Fix env

--- src/env_config.js
+++ src/env_config.js
@@ -1,13 +1,19 @@
 const dotenv = require('dotenv');
-const result = dotenv.config({
-    silent: true,
-    path: __dirname + '/.env'
+dotenv.config({
+    silent: true
 });
 
-console.log(__dirname);
-if (result.error) {
-    throw result.error;
-}
+// console.log(__dirname);
+// if (result.error) {
+//     throw result.error;
+// }
 
-const { parsed: envs } = result;
-module.exports = envs;
+// const { parsed: envs } = result;
+module.exports = {
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PWD: process.env.DB_PWD,
+    DB_PORT: process.env.DB_PORT,
+    PORT: process.env.PORT
+};
